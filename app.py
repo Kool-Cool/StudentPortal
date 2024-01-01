35ca72249a8c54e5a7222fe8ec8e66f85100bf70
Kool-Cool <omgrabpoint@gmail.com>
2024-01-01 13:49:06 +0530

    Updated Templates

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
 # Info form se aage nahi jaa rahi :)
 from flask import Flask , render_template , request
 
-
 from db_info import dummy_database as ddb
-from db_info import admin_dummy_database as addb
+from db_info import admin_dummy_database as adb
+
 
 app = Flask(__name__)
 
+
+@app.route("/try")
+def myTry():
+    return render_template("try.html")
+
+
+
 @app.route("/" , methods = ["POST" , "GET"])
 def home():
-    # return "This is Home Page !!"
-    return render_template("login.html")
+    return render_template("std_login.html")
 
-@app.route("/student_login" , methods = ["POST"])
-def login():
+@app.route("/student_dashbord" , methods = ["POST"])
+def std_login():
     student_email = request.form["student_email"]
     student_password = request.form["student_password"]
     
     if student_email not in ddb:
-        return render_template("login.html" , return_message = "Invalid User")
+        return render_template("std_login.html" , return_message = "Invalid User")
     else:
         if ddb[student_email] != student_password:
-            return render_template("login.html" , return_message = "Invalid Password")
+            return render_template("std_login.html" , return_message = "Invalid Password")
         else:
-            return render_template("std_home.html" , student_email = student_email)
+            return render_template("std_dashboard.html" , student_email = student_email)
 
 
 
-@app.route("/admin/login" , methods = ["POST"])
+@app.route("/admin/login" )
 def admin_login():
     # if person (instructor) is_role = Instuctor  , he / she has extra privillages
     # Adding class post or deleting class post , sharing files 
@@ -35,19 +41,17 @@ def admin_login():
     return render_template("admin_login.html")
 
 
-@app.route("/admin/login/<email>" , methods = ["POST"])
-def admin_dashboard(email):
-        return f"This is dashbord for {email}"
-    # admin_email = request.form["admin_email"]
-    # admin_password = request.form["admin_password"]
+@app.route("/admin_dashbord" ,methods = ["POST"])
+def admin_dashboard():
+    admin_email = request.form["admin_email"]
+    admin_password = request.form["admin_password"]
     
-    # if admin_email not in addb:
-    #     return render_template("admin_login.html" , return_message = "Invalid User")
-    # else:
-    #     if addb[admin_email] != admin_password:
-    #         return render_template("admin_login.html" , return_message = "Invalid Password")
-    #     else:
-    #         # return render_template('', admin_email = admin_email)   
-    #         return f"this is dashborad for admin {admin_email}"
+    if admin_email not in adb:
+        return render_template("admin_login.html" , return_message = "Invalid User")
+    else:
+        if adb[admin_email] != admin_password:
+            return render_template("admin_login.html" , return_message = "Invalid Password")
+        else:
+            return render_template("admin_dashboard.html" , admin_email = admin_email)
 
     
--- db_info.py
+++ db_info.py
@@ -13,17 +13,11 @@ admin_dummy_database = {
 # DataBase Schema
 
 """  
-- takes:        ID, course_id, sec_id, semester, year, grade
-- student:      ID, name, dept_name, tot_cred
-- section:      course_id, sec_id, semester, year, building, room_no, time_slot_id
-- course:       course id, title, dept_name, credits
-- time_slot:    time_slot_id, day, start_time, end_time
-- classroom:    building, room_no, capacity
-- teaches:      ID, course_id, sec_id, semester, year
-- instructor:   ID, name, dept_name, salary
-
 
 - admin: ID , name , email , password
+
+
+
 
 """
 
